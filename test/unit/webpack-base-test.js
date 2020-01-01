3d4ab4f79f74098795b7db85ccee3af738098af6
Wang Peng <947034046@qq.com>
2020-01-01 19:15:49 +0800

    test: webpack.base test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -7,7 +7,7 @@ describe('webpack.base.js test case', () => {
   console.log(baseConfig);
 
   it('entry', () => {
-    assert.equal(baseConfig.entry.index, 'C:/Users/Administrator/Projects/webpack/my-project/demo1/builder-webpack/test/smoke/template/src/index/index.js');
-    assert.equal(baseConfig.entry.search, 'C:/Users/Administrator/Projects/webpack/my-project/demo1/builder-webpack/test/smoke/template/src/search/index.js');
+    assert.equal(baseConfig.entry.index.includes('builder-webpack/test/smoke/template/src/index/index.js'), true);
+    assert.equal(baseConfig.entry.search.includes('builder-webpack/test/smoke/template/src/search/index.js'), true);
   });
 });
